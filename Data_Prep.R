54e1c7e9b0618e9f9b02bf62d8e7227ded5f016e
Gregory Chick <gregchick1@gmail.com>
2021-10-10 23:02:45 -0400

    Actual IPEDs data

--- Data_Prep.R
+++ Data_Prep.R
@@ -1,5 +1,10 @@
 #Load packages=====
 library(tidyverse)
+#library(DRDID) #Confirm if this is appropriate estimator for this project
+#library(performance) #Use to check model fit, use check_model() function
+install.packages("performance")
+
+
 
 #Load data====
 df_2011 <- read_csv("~/Downloads/CSV_882021-409/CSV_882021-409.csv")
